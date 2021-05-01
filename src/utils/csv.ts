93af61499ef5cbf62e13e8a257cbda6e9a100bae
eggplantiny <eggplantiny@gmail.com>
2021-05-01 22:25:48 +0900

    csv 모듈 개발중:

---
+++ src/utils/csv.ts
@@ -0,0 +1,6 @@
+const rows = [
+    ["name1", "city1", "some other info"],
+    ["name2", "city2", "more info"]
+];
+
+let csvContent = "data:text/csv;charset=utf-8," + rows.map(e => e.join(",")).join("\n");
